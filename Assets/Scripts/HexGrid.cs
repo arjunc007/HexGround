5bddf57f2eebacfc524eb7860311cb92b966f510
arjunc007 <arjunchauhan007@gmail.com>
2017-07-30 10:39:38 +0530

    Added Initial Animation
    Figured out how to change color of a tile

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -8,13 +8,15 @@ public class HexGrid : MonoBehaviour {
 
 	public int x = 5;
 	public int y = 5;
-
-	public float radius = 0.5f;
 	public float separation = 0.01f;
 
+	private Ray ray = new Ray();
 	private Transform boardHolder;
 	private List<Tile> tileList;
 	private float offsetX, offsetY;
+
+	public const float outerRadius = 0.5f;
+	public const float innerRadius = outerRadius * 0.866025404f; //Root 3 / 2
 
 	void Awake() 
 	{
@@ -28,7 +30,21 @@ public class HexGrid : MonoBehaviour {
 		SetupBoard ();
 
 		for (int i = 0; i < tileList.Count; i++)
-			tileList [i].FindNeighbours ();
+			tileList [i].SetupTile ();
+	}
+
+	void Update()
+	{
+		
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Tile clickedTile = GetClickedTile (Input.mousePosition);
+
+			if (clickedTile != null) 
+			{
+				clickedTile.ChangeColor ();
+			}
+		}
 	}
 
 	//Sets up a grid of hexagons
@@ -36,10 +52,10 @@ public class HexGrid : MonoBehaviour {
 	{
 		boardHolder = new GameObject ("Board").transform;
 
-		float unitLength = radius + separation;
+		float offset = outerRadius + separation;
 
-		offsetX = unitLength * Mathf.Sqrt(3);
-		offsetY = unitLength * 1.5f;
+		offsetX = offset * Mathf.Sqrt(3);
+		offsetY = offset * 1.5f;
 
 		for( int i = 1; i < x + 1; i++ ) {
 			for( int j = 1; j < y + 1; j++ ) {
@@ -66,5 +82,17 @@ public class HexGrid : MonoBehaviour {
 
 		position.y = y * offsetY;
 		return position;
+	}
+
+	private Tile GetClickedTile(Vector3 mousePosition)
+	{
+		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
+
+		if (hit.collider != null) 
+		{
+			return hit.collider.GetComponent<Tile> ();
+		}
+
+		return null;
 	}
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,32 +6,55 @@ public class Tile : MonoBehaviour {
 
 	public List<GameObject> neighbourList = new List<GameObject> ();
 
-	//private CircleCollider2D circleCollider2D;
+	private CircleCollider2D circleCollider2D;
 
-	//private int owner = 0;
-	//private Color color = Color.grey;
+	private int owner = 0;
+	private Animator anim;
+	private Renderer renderer;
 
-	//void Start()
-	//{
-	//	sideLength = HexGrid.instance.radius;
-	//}
-
-	public void FindNeighbours()
+	public void SetupTile()
 	{
-		Collider2D[] hitColliders = Physics2D.OverlapCircleAll (transform.position, HexGrid.instance.radius);
+		anim = GetComponent<Animator> ();
+		renderer = GetComponent<Renderer> ();
 
-		Debug.Log (hitColliders.Length);
+		circleCollider2D = GetComponent<CircleCollider2D> ();
+		circleCollider2D.radius = HexGrid.outerRadius;
+
+		FindNeighbours();
+
+		circleCollider2D.radius = HexGrid.innerRadius; 
+	}
+
+	private void FindNeighbours()
+	{
+		Collider2D[] hitColliders = Physics2D.OverlapCircleAll (transform.position, HexGrid.outerRadius);
 
 		foreach(Collider2D collider in hitColliders)
 		{
-			Debug.Log (collider.transform.position);
 			//Debug.Log ("Enter foreach");
 			if (collider.name == "HexagonTile(Clone)" && collider.transform.position != transform.position) 
 			{
-				Debug.Log ("Enter if");
 				neighbourList.Add (collider.gameObject);
-				Debug.Log ("Add to list");
 			}
+		}
+	}
+
+	public void ChangeColor()
+	{
+		Debug.Log (owner);
+
+		if (owner == 0) 
+		{
+			anim.SetTrigger ("GrayToBlue");
+			owner = 1;
+			renderer.material.color = Color.blue;
+
+		} else if (owner == 1) 
+		{
+			Debug.Log ("Blue to Gray");
+			anim.SetTrigger ("BlueToGray");
+			owner = 0;
+			renderer.material.color = Color.gray;
 		}
 	}
 }
